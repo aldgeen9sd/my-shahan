71e989c6f0ec9d8285e24836a04ca9699b480e34
aldgeen9sd <fatmhalshlyf7@gmail.com>
2025-06-30 23:59:19 +0300

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,166 @@
+<?php include"files/header.php" ;?>
+           <ul>
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/images (3).jpeg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> سماعه بلوتوث </a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>             
+                
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/images (2).jpeg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> عطر سوفاج ديور</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/images (5).jpeg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> تلفون ايفون</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230211-WA0018.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية قلب وحيد القرن</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230211-WA0018.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230211-WA0018.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/images (3).jpeg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> سماعه بلوتوث </a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li><li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230501-WA0045.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية عزيري قلب </a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li><li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230319-WA0027.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> بدلات تخرج</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li><li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230131-WA0061.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية نجرانية صيفان </a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230211-WA0018.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> جنبية عزيري صيفان</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+                <li>
+                    <div class="product ">
+                        <div id="pro_img">
+                            <a href="#"><img src="images/IMG-20230416-WA0001.jpg" width="320" height="150" alt=""></a>
+                        </div>
+                        <div id="pro_title">
+                            <a href="#"> شيلان صوف</a>
+                        </div>
+                        <div id="pro_bay">
+                            <a href="#"><button>شراء الان</button></a>
+                        </div>
+                    </div>
+                </li>
+
+                <div class="c"></div>
+
+                
+            </ul>
+            <?php include"files/footer.php" ;?>
